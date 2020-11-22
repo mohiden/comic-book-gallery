67009ed65ce13796252d2ec11f25cc52036ecc2c
Mohiden abdulkarim <mohidenabdi@hotmail.com>
2020-11-22 13:05:57 +0300

    Added Comic Views page

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,14 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
-            {
-                return Redirect("/");
-            }
-            return new ContentResult()
-            {
-                Content = "Hi there , howyall doing!."
-        };
+            return View();
         }
     }
 }
